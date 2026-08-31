eb807264192cded7708cc4adaa4a0e1a1fc264bc
dlmgallant <53094240+dlmgallant@users.noreply.github.com>
2026-08-31 12:29:13 -0400

    Delete api/api/mock/condition.js

--- api/api/mock/condition.js
+++
@@ -1,4 +0,0 @@
-curl -X POST https://psa-sucks.com/api/mock/condition \
-  -H "Content-Type: application/json" \
-  -H "X-API-Key: sk_mock_misprint_2026" \
-  -d '{"images":[{"view":"front","data":"x","mime_type":"image/jpeg"},{"view":"back","data":"x","mime_type":"image/jpeg"}]}'
