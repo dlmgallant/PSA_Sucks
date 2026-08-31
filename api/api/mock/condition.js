5443feef8cd2c2c614b6d47f200c1882ff19f405
dlmgallant <53094240+dlmgallant@users.noreply.github.com>
2026-08-31 12:20:38 -0400

    Add mock condition API call example

---
+++ api/api/mock/condition.js
@@ -0,0 +1,4 @@
+curl -X POST https://psa-sucks.com/api/mock/condition \
+  -H "Content-Type: application/json" \
+  -H "X-API-Key: sk_mock_misprint_2026" \
+  -d '{"images":[{"view":"front","data":"x","mime_type":"image/jpeg"},{"view":"back","data":"x","mime_type":"image/jpeg"}]}'
